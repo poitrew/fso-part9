02a938a56132ce78b8f984349156a2df8da1257e
poitrew <dtdat101101@gmail.com>
2022-07-08 21:42:11 +0700

    done 9.6, 9.7

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,30 +8,22 @@ interface Result {
   average: number
 }
 
-const [ firstArg, secondArg, targetInput, ...days] = process.argv
+export const calculateExercises = (target: number, dailyExercises: number[]): Result => {
+  if (dailyExercises.some(e => isNaN(e))) throw new Error('Training days have to be number values (hours)');
+  if (dailyExercises.length < 1) throw new Error('Need at least one day for calculation');
+  if (isNaN(target)) throw new Error('Target must be a number');
 
-const calculateExercises = (arr: number[]): Result => {
-  if (arr.some(e => isNaN(e))) throw new Error('Training days have to be number values (hours)')
-  if (arr.length < 1) throw new Error('Need at least one day for calculation')
-
-  const total = arr.reduce((total, current) => total += current, 0)
-  const average = total / arr.length
-  const target = Number(targetInput)
-  const rating = Math.floor((average / target) * 3)
+  const total = dailyExercises.reduce((total, current) => total += current, 0);
+  const average = total / dailyExercises.length;
+  const rating = Math.floor((average / target) * 3);
 
   return {
-    periodLength: arr.length,
-    trainingDay: arr.filter(d => d !== 0).length,
+    periodLength: dailyExercises.length,
+    trainingDay: dailyExercises.filter(d => d !== 0).length,
     success: average > target,
     rating,
     ratingDescription: rating < 2 ? 'too bad' : rating > 2 ? 'good' : 'not too bad but could be better',  
     target,
     average,
-  }
-} 
-
-try {
-  console.log(calculateExercises(days.map(d => Number(d))))
-} catch(err) {
-  console.error('Error:', err.message)
-}
+  };
+}; 
--- index.ts
+++ index.ts
@@ -1,27 +1,45 @@
-import express from 'express'
+import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
-const app = express()
+const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const { height, weight } = req.query
+  const { height, weight } = req.query;
   try {
-    const bmi = calculateBmi(Number(height), Number(weight))
+    const bmi = calculateBmi(Number(height), Number(weight));
     res.json({
       height,
       weight,
       bmi
-    })
+    });
   } catch (error) {
-    res.status(401).json({ error: 'malformatted parameters '})
+    res.status(401).json({ error: 'malformatted parameters '});
   }
-})
+});
 
-const PORT = 3002
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+  if (!(daily_exercises && target)) {
+    res.status(401).json({ error: 'parameters missing' });
+    return;
+  }
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculateExercises(target, daily_exercises);
+    res.json(result);
+  } catch (error) {
+    res.status(401).json({ error: 'malformatted parameters' });
+  }
+});
+
+const PORT = 3002;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
